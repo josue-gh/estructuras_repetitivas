19ba858ee453492b0de354edc6e2cf4730464cdc
josue <josue.mlpro.2014@gmail.com>
2025-05-30 21:20:47 -0500

    Este es mi tercer archivo de repetitivas

---
+++ Divicion_De_Un_Numero.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n1, n2, s, c;
+	
+	cout<<ingrese un numero mayor <<endl;
+	cin>> n1;
+	
+	cout<<ingrese un numero menor <<endl;
+	cin>> n2;
+	
+	s= n1;
+	
+	c=0;
+	
+	while(s>=n2){
+		
+		s=s-n2;
+		
+		c=c+1;
+		
+	}
+	
+	cout<< c<<endl;
+	cout<<s;
+	
+	return 0;
+}
